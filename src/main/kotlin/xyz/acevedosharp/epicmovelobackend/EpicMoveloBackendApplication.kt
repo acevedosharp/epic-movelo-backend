fff810ed2cf7312117fd102420d3ef5bd9edc576
acevedosharp <acevedosharp@hotmail.com>
2020-11-06 07:21:00 -0500

    Initial commit

---
+++ src/main/kotlin/xyz/acevedosharp/epicmovelobackend/EpicMoveloBackendApplication.kt
@@ -0,0 +1,11 @@
+package xyz.acevedosharp.epicmovelobackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EpicMoveloBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<EpicMoveloBackendApplication>(*args)
+}
---
+++ src/test/kotlin/xyz/acevedosharp/epicmovelobackend/EpicMoveloBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package xyz.acevedosharp.epicmovelobackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EpicMoveloBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
